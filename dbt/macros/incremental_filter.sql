6117f8791bc0e0123073c55cae4ab9db6460b715
Brightpmk <bright19699@gmail.com>
2026-05-26 23:08:38 +0700

    feat: add incremental_filter macro to handle dynamic lookback intervals for incremental models

--- dbt/macros/incremental_filter.sql
+++ dbt/macros/incremental_filter.sql
@@ -1,5 +1,6 @@
 {% macro incremental_filter(outer_column, inner_column, lookback_days=3) %}
   {% if is_incremental() %}
-    where {{ outer_column }} >= (select max({{ inner_column }}) - interval '{{ lookback_days }} days' from {{ this }})
+    where {{ outer_column }} >= coalesce((select max({{ inner_column }}) - interval '{{ lookback_days }} days' from {{ this }}), '1970-01-01'::timestamp)
   {% endif %}
 {% endmacro %}
+
